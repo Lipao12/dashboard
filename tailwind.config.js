9d5dd5eaa2c4a54a6cf42088959693ac26083ab1
Filipe Mai <f.nunesmai19@gmail.com>
2024-09-05 15:42:40 -0300

    Initial config

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+    colors:{
+      main: "#fafafa",
+      backgroud: "#09090b",
+      secundary: "#242933",
+      terciary: "#ededed"
+    }
+    },
+  },
+  plugins: [],
+}
